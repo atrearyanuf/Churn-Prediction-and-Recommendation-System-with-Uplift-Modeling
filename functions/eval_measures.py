5a5da05fc884e334418fbaab7fd8b9d62ddae76d
Aryan Atre <atrearyan@ufl.edu>
2026-03-01 23:59:10 -0500

    gitignore

--- functions/eval_measures.py
+++ functions/eval_measures.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 Code for the paper
 "A churn prediction dataset from the telecom sector: a new benchmark for uplift modeling"
-Anonymous authors
 """
 
 import numpy as np
@@ -26,9 +24,6 @@ def uplift_curve(y, t, score, use_churn_convention=True):
 
 def profit_curve(y, t, score, CB=np.array([[1, 1], [0, 0]])):
     """Generalization of the uplift curve to arbitrary cost-benefits.
-    Based on the paper:
-    Verbeke, Wouter, et al. "The foundations of cost-sensitive causal
-    classification." arXiv preprint arXiv:2007.12582 (2020).
     """
     score_ranking = np.argsort(score)[::-1]
     y = y[score_ranking]
@@ -59,13 +54,7 @@ def profit_curve(y, t, score, CB=np.array([[1, 1], [0, 0]])):
     return grid
 
 def calibrate_score(score, p):
-    """
-    Calibration of posterior probabilities as shown in
-    Dal Pozzolo, Andrea, et al. "Calibrating probability
-    with undersampling for unbalanced classification."
-    2015 IEEE Symposium Series on Computational Intelligence.
-    IEEE, 2015.
-    """
+
     p_s = np.mean(score)
     ratio = p * (p_s - 1) / (p_s * (p - 1))
     return ratio * score / (ratio * score - score + 1)
